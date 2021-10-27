ff1851d3d793fc6c75f3b427558b74da163aee4f
Diluted666 <jacoszekwojciech@gmail.com>
2021-10-27 12:18:26 +0200

    Initial setup

---
+++ spring-backend/src/main/java/com/delly/delly/DellyApplication.java
@@ -0,0 +1,13 @@
+package com.delly.delly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DellyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DellyApplication.class, args);
+    }
+
+}
---
+++ spring-backend/src/test/java/com/delly/delly/DellyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delly.delly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DellyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
