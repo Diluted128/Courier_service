d1a9245f833e47479ce76b07b5567fa9b64f9fe7
Diluted666 <jacoszekwojciech@gmail.com>
2021-11-25 14:55:00 +0100

    route of lat and lng and graphics update

--- react-frontend/delly/src/components/map/map.js
+++ react-frontend/delly/src/components/map/map.js
@@ -1,133 +1,126 @@
-import * as React from "react"
-import '../../stylesheets/map/map.css'
-import Test from '../map/test.jsx'
+import * as React from "react";
+import "../../stylesheets/map/map.css";
+import Test from "../map/test.jsx";
+import Box from "../../images/box_local_icon.png"
+import Meta from "../../images/meta_icon.png"
 
-class map extends React.Component{
+class map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+    this.map = null;
+    this.H = null;
+    this.platform = null;
+  }
 
-    state = {
-        map: null,
-        platform: null,
-        H: null,
-        routingService: null
-    }
+  componentDidMount() {
+    const H = window.H;
 
-    mapRef = React.createRef();
+    const platform = new H.service.Platform({
+      apikey: "tPpNaaUaiKemvwf0c7oBk8L04D7iJfanL94O4PkXUaU",
+    });
 
-    componentDidMount(){
-        
-        var H = window.H;
-        var platform = new H.service.Platform({
-            apikey: "tPpNaaUaiKemvwf0c7oBk8L04D7iJfanL94O4PkXUaU"
-        });
+    const defaultLayers = platform.createDefaultLayers();
 
-        var defaultLayers = platform.createDefaultLayers();
-        
-        var map = new H.Map(
-            this.mapRef.current,
-            defaultLayers.vector.normal.map,
-            {
-              // This map is centered over Europe
-              center: { lat: 50, lng: 5 },
-              zoom: 4,
-            }
-        );
-        
-        const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+    const map = new H.Map(
+      this.ref.current,
+      defaultLayers.vector.normal.map,
+      {
+        center: { lat: 50.06139, lng: 19.93800 },
+        zoom: 13,
+      }
+    );
 
-        const ui = H.ui.UI.createDefault(map, defaultLayers);
+    const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
-        // map.dispose();
+    const ui = H.ui.UI.createDefault(map, defaultLayers);
 
-        var routingService = platform.getRoutingService(null, 8);
-       
-       // 'origin': '49.64008, 19.32883',
-       // 'destination': '50.00627, 19.87327',
-        routingService.calculateRoute(
-                {
-                    'routingMode': 'fast',
-                    'transportMode': 'car',
-                    // The start point of the route:
-                    'origin': '49.640080000000,19.3288300000',
-                    // The end point of the route:
-                    'destination': '50.00627,19.87327',
-                    // Include the route shape in the response
-                    'return': 'polyline'
+    this.map = map;
+    this.H = H;
+    this.platform = platform;
+
+    // var one = new H.map.Marker({ lat: 49.64008 + 1, lng: 19.32883 + 1 });
+    // var two = new H.map.Marker({ lat: 50.00627 + 1, lng: 19.87327 + 1 });
+    // var groupp = new H.map.Group();
+    // groupp.addObjects([one, two]);
+    // map.getViewModel().setLookAtData({
+    //   bounds: groupp.getBoundingBox(),
+    // });
+  }
+
+
+  getRoutingDetails = (from, to) => {
+      from = "50.07149,19.93346";
+      to = "50.04581,19.95240";
+
+      return (
+        {
+            routingMode: "fast",
+            transportMode: "pedestrian",
+            origin: from,
+            destination: to,
+            return: 'polyline,turnByTurnActions,actions,instructions,travelSummary'
+          }
+      )
+  }
+
+//   origin: "50.07149,19.93346",
+//   destination: "50.04581,19.95240",
+  successThenDrawRoute = (result) => {
     
-                },
-                result => {
-                    if (result.routes.length) {
-                        result.routes[0].sections.forEach((section) => {
-                             // Create a linestring to use as a point source for the route line
-                            let linestring = H.geo.LineString.fromFlexiblePolyline(section.polyline);
-                    
-                            // Create a polyline to display the route:
-                            let routeLine = new H.map.Polyline(linestring, {
-                              style: { strokeColor: 'blue', lineWidth: 3 }
-                            });
-                    
-                            // Create a marker for the start point:
-                            let startMarker = new H.map.Marker(section.departure.place.location);
-                    
-                            // Create a marker for the end point:
-                            let endMarker = new H.map.Marker(section.arrival.place.location);
-                    
-                            // Add the route polyline and the two markers to the map:
-                            map.addObjects([routeLine, startMarker, endMarker]);
-                    
-                            // Set the map's viewport to make the whole route visible:
-                            map.getViewModel().setLookAtData({bounds: routeLine.getBoundingBox()});
-                        });
-                      }
-                },
-                error => {
-                    console.error(error);
-                }
-            )    
+        if (result.routes.length) {
+          result.routes[0].sections.forEach((section) => {
+            // Create a linestring to use as a point source for the route line
+            let linestring = this.H.geo.LineString.fromFlexiblePolyline(
+              section.polyline
+            );
 
-    }
+            // Create a polyline to display the route:
+            let routeLine = new this.H.map.Polyline(linestring, {
+              style: { strokeColor: "rgba(73, 73, 73, 0.8)", lineWidth: 6 },
+            });
 
-    drawRoute = () => {
+            var startIcon = new this.H.map.Icon(Box);
+            // Create a marker for the start point:
+            let startMarker = new this.H.map.Marker( section.departure.place.location, {icon: startIcon});
 
-        var start = {
-            lat: "37",
-            lng: "-121"
+            var endIcon = new this.H.map.Icon(Meta);
+            // Create a marker for the end point:
+            let endMarker = new this.H.map.Marker(section.arrival.place.location, {icon: endIcon});
+
+            // Add the route polyline and the two markers to the map:
+            this.map.addObjects([routeLine, startMarker, endMarker]);
+
+            // Set the map's viewport to make the whole route visible:
+            this.map
+              .getViewModel()
+              .setLookAtData({ bounds: routeLine.getBoundingBox() });
+          });
         }
+  }
 
-        var finish = {
-            lat: "38",
-            lng: "-122"
-        }
+  drawRoute = (from, to) => {
+      
+    var routingService = this.platform.getRoutingService(null, 8);
 
-        console.log(this.state.routingService)
-        this.state.routingService.calculateRoute(
-            {
-                'routingMode': 'fast',
-                'transportMode': 'car',
-                 "origin": '50.1120423728813,8.68340740740811',
-                 "destination": '52.5309916298853,13.3846220493377',
-                 'return': 'polyline'
+    routingService.calculateRoute(
+        this.getRoutingDetails(from, to),
+        this.successThenDrawRoute, 
+        (error) => {
+        console.error(error);
+      }
+    )
+  }
 
-            },
-            data => {
-                console.log("udalo sie nice");
-            },
-            error => {
-                console.error(error.message);
-            }
-        )
-    }
+  render() {
+    return (
 
-    render(){
-        return(
-            <>
-            <Test />
-            <div ref={this.mapRef} style={{ height: "500px" }} />
-            </>
-        )
-    }
+        <div>
+        <Test drawRoute={this.drawRoute}/>
+        <div ref={this.ref} class="map" style={{ height: "800px" }} />
+        </div>
+    );
+  }
 }
 
-
 export default map;
-
-
--- react-frontend/delly/src/components/map/test.jsx
+++ react-frontend/delly/src/components/map/test.jsx
@@ -1,10 +1,26 @@
 import '../../stylesheets/map/test.css'
 import React,{Component} from "react";
-
 class test extends Component{
+
+    constructor(props){
+      super(props)
+      this.state = {
+        from: "",
+        to: ""
+      }
+      this.sOnClickk = this.OnClickk.bind(this);
+
+    }
+
+    OnClickk = function(){
+    
+      console.log("dziala")
+        // console.log("from: " + this.state.from + " to:" + this.state.to)
+    }
 
     render(){
         return(
+          <>
             <div class="jumbotron">
             <div class="containter-fluid">
               <h1>Find the Distance Betweeen Two Places</h1>
@@ -16,28 +32,21 @@ class test extends Component{
                       <i class="fas fa-map-marker-alt icon"></i>                  
                     </label>
                      <div class="col-xs-4">
-                       <input type="text" class="form-control" id="from" placeholder="Origin"/>
+                       <input type="text" onChange={event => this.state.from = event.target.value}class="form-control" id="from"/>
                      </div>
                     
                      <label for="to" class="col-xs-2-control-label">
                       <i class="fas fa-map-marked-alt icon"></i>
                      </label>
                      <div class="col-xs-4">
-                       <input type="text" class="form-control" id="to" placeholder="Desination"/>
+                       <input type="text" onChange={event => this.state.to = event.target.value} class="form-control" id="to"/>
                      </div>
                 </div>
               </form>
-              <div class="col-xs-offset-2 col-xs-10">
-                  
-              </div>
             </div>
-            <div class="contrainter-fluif">
-              <div id="map">
-              </div>
-
-              <div id="output"></div>
-              </div>
-            </div>
+            </div>  
+            <button onClick={() => this.props.drawRoute(this.state.from , this.state.to)}>draw</button>
+            </>
         )
     }
 } export default test;
