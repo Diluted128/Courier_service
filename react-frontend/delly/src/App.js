f90b4cf9ad86258ed73a2069b49513004751f2e8
Diluted666 <jacoszekwojciech@gmail.com>
2021-12-07 20:47:22 +0100

    Added login page

--- react-frontend/delly/src/App.js
+++ react-frontend/delly/src/App.js
@@ -6,33 +6,15 @@ import Test from './components/map/test.jsx'
 import {DistanceMatrixService} from "react-google-maps"
 import HomePage from "./components/homePage/homePage.jsx"
 import { Component } from 'react'
+import Login from "./components/Pop'up/Login.jsx"
 
 class App extends Component{
-
-  state = {
-    calcDistance: null
-  }
-
-  // calcDistance = () => {
-
-  //   google
-  //   <DistanceMatrixService
-  //   options={{
-  //     destinations: [{lat:1.296788, lng:103.778961}],
-  //     origins: [{lng:103.780267, lat:1.291692}],
-  //     travelMode: "DRIVING",
-  //   }}
-  
-  //   callback = {(response) => {console.log(response)}}
-  //   />
-  // }
-
-
 
   render(){
   return (
     <>
      {/* <Map/> */}
+    {/* <HomePage/> */}
     <HomePage/>
     </>
   );
--- react-frontend/delly/src/components/homePage/homePage.jsx
+++ react-frontend/delly/src/components/homePage/homePage.jsx
@@ -1,4 +1,4 @@
-import React,{Component, useState} from "react";
+import React,{Component} from "react";
 import "../../stylesheets/homePage/homePage.css"
 import BoxLogo from "../../images/homePage/boxlogo.png"
 import Lock from "../../images/homePage/lock.png"
@@ -13,13 +13,24 @@ import Splash1 from "../../images/homePage/splash-1.png"
 import Splash2 from "../../images/homePage/splash-2.png"
 import Splash3 from "../../images/homePage/splash-3.png"
 import WhiteLogo from "../../images/homePage/box-logo-white.png"
+import MapImg from "../../images/homePage/map-img.png"
+import LoginPopup from "../Pop'up/Login.jsx"
+
 class HomePage extends Component{
+
+   constructor(){
+      super();
+      this.state={
+         open: false
+      }
+   }
+
    render(){
        return(
-          <div class="main-container">
-          <section class="section1">
+          <div className="main-container">
+          <section className="section1">
           <span id="circle-section1"></span>   
-           <div class="container-fluid my-container">
+           <div className="container-fluid my-container">
                <div className="col row1">
                  <div className="d-flex flex-row f-row" >
                 <div className="nav-item-logo">
@@ -37,7 +48,7 @@ class HomePage extends Component{
                     <a href="#Lokalizacja" className="nav-text">Kontakt</a>
                 </div>
                 <div className="p-3 ml-auto p-2 nav-item">
-                <img src={Lock} id="lock-img" alt="Lock"/>
+                <input onClick={() => this.setState({open: true})} type="image" src={Lock} id="lock-img"/>   
                 </div>
                </div>
               </div>
@@ -47,27 +58,28 @@ class HomePage extends Component{
               <span id="text-2">Supermarkety, sklepy, apteki, cokolwiek <br/>potrzebujesz!</span>   
               </div> 
               <img src={deliveryMan} id="delivery-man-img" alt="deliveryman"/> 
-              <button class="btn btn-primary rounded-pill" id="start-button" type="submit">Rozpocznij</button>
+              <button onClick={ () => this.setState({open: true})} className="btn btn-primary rounded-pill" id="start-button" type="submit">Rozpocznij</button>
+              <LoginPopup openPopup={this.state.open} closePopup={() => this.setState({open: false})}/>
               </div>
           </div>
           </section>
-          <section id="Oferta" class="section2">
+          <section id="Oferta" className="section2">
 
           <img src={HalfCircle} id="half-circle-img" alt="deliveryman"/> 
             <span id="title-section2">Dostarczymy wszystko</span>   
 
-            <div class="d-flex flex-row justify-content-center flex-container2">
-            <div class="nav-item2">
+            <div className="d-flex flex-row justify-content-center flex-container2">
+            <div className="nav-item2">
                <img src={Restaurant} id="sec2-img1" alt="Lock"/>
                <span className="sec2-text1">Popularne restauracje w <br/>Twoim mieście<br/></span>
                <span className="sec2-text2">Dzięki szerokiej ofercie restauracji<br/> możesz zamówić swoje ulubione<br/> jedzenie.</span>
             </div>
-            <div class="nav-item2">
+            <div className="nav-item2">
                <img src={Delivery} id="sec2-img2" alt="Lock"/>
-               <spam className="sec2-text1">Szybka dostawa<br/></spam>
+               <span className="sec2-text1">Szybka dostawa<br/></span>
                <span className="sec2-text2">Szybka dostawa jest dla nas<br/> kluczowa. Zamów lub wyślij<br/> dowolny artykuł w swoim mieście.</span>
             </div>
-            <div class="nav-item2">
+            <div className="nav-item2">
                <img src={Grossery} id="sec2-img3" alt="Lock"/>
                <span className="sec2-text1">Dostawa artykułów <br/>spożywczych i nie tylko<br/></span>
                <span className="sec2-text2">Z nami znajdziesz wszystko! Od<br/> supermarketów po sklepy, apteki i<br/> kwiaciarnie — jeśli punkt działa w<br/> Twoim mieście, zajmiemy się dostawą.</span>
@@ -77,51 +89,52 @@ class HomePage extends Component{
             <img src={Wave} id="wave-img" alt="deliveryman"/>
 
           </section>
-          <section id="Wspolpraca" class="section3">
+          <section id="Wspolpraca" className="section3">
 
           <span id="title-section3">Razem możemy więcej!</span> 
 
-          <div class="d-flex flex-row justify-content-center flex-container3">
-            <div class="nav-item3">
+          <div className="d-flex flex-row justify-content-center flex-container3">
+            <div className="nav-item3">
                <img src={Splash1} id="sec3-img1" alt="Lock"/>
                <span className="sec3-text1">Zostań kurierem<br/></span>
                <span className="sec3-text2">Nie potrzebujesz szefa! Korzystaj z <br/>elastyczności, swobody i zarobków,<br/> które umożliwia Delly..</span>
             </div>
-            <div class="nav-item3">
+            <div className="nav-item3">
                <img src={Splash2} id="sec3-img2" alt="Lock"/>
                <spam className="sec3-text1">Zostań Pracownikiem<br/></spam>
                <span className="sec3-text2">Szukasz ciekawej pracy? Jeśli<br/> uwielbiasz wyzwania, pomaganie i<br/> kontakt z ludźmi, skontaktuj się z<br/> nami!</span>
             </div>
-            <div class="nav-item3">
+            <div className="nav-item3">
                <img src={Splash3} id="sec3-img3" alt="Lock"/>
                <span className="sec3-text1">Zostań partnerem<br/></span>
                <span className="sec3-text2">Rozwijaj działalność z pomocą<br/> Delly! Nasze rozwiązania i baza<br/> użytkowników pomagają<br/> zwiększyć sprzedaż i poszerzać<br/> klientelę!</span>
             </div>
             </div>
           </section>
-          <section id="Lokalizacja" class="section4">
+          <section id="Lokalizacja" className="section4">
           <img src={WaveFlipped} id="wave-flipped-img" alt="deliveryman"/> 
           <div id="logo2">
           <img src={WhiteLogo} id="logo-white-img" alt="Box logo"/>
           <span id="boxLogo-white">Delly</span>
           </div>
           <div id="list">
-             <div class="d-flex flex-row flex-container4">
+             <div className="d-flex flex-row flex-container4">
                  <div className="nav-item4"><spam>Współpraca</spam></div>
                  <div className="nav-item4"><spam>Przydatne linki</spam></div>
                  <div className="nav-item4"><spam>Obserwuj nas</spam></div>
              </div>
-             <div class="d-flex flex-row flex-container5">
+             <div className="d-flex flex-row flex-container5">
                  <div className="nav-item5"><spam>Delly dla partnerów</spam></div>
                  <div className="nav-item5"><spam>O nas</spam></div>
                  <div className="nav-item5"><spam>Facebook</spam></div>
              </div>
-             <div class="d-flex flex-row flex-container6">
+             <div className="d-flex flex-row flex-container6">
                  <div className="nav-item5"><spam>Kurierzy</spam></div>
                  <div className="nav-item5"><spam>FAQ</spam></div>
                  <div className="nav-item5"><spam>Instagram</spam></div>
              </div>
              </div>
+          <img src={MapImg} id="map-img" alt="Box logo"/>  
           </section>
           </div>
        )
