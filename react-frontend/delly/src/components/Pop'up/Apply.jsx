6f3854364f5ddc40023f0f8ec8450b51fb4d4f1d
Diluted666 <jacoszekwojciech@gmail.com>
2021-12-13 16:23:47 +0100

    Changed css to SASS with BEN methodology

--- react-frontend/delly/src/components/Pop'up/Apply.jsx
+++ react-frontend/delly/src/components/Pop'up/Apply.jsx
@@ -1,51 +1,98 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import ReactDom from "react-dom";
-import "../../stylesheets/Pop'up/Apply.css"
-import Cross from "../../images/svg/cross.svg"
-import reactDom from "react-dom";
+import "../../stylesheets/Pop'up/Apply.scss";
+import Cross from "../../images/svg/cross.svg";
 
-class Apply extends Component{
+class Apply extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
-    
-    render(){
-
-        if(this.props.openPopup == false) return null;
-        return ReactDom.createPortal(
-            <div id="gray-cover">
-            <div id="login-container-2"> 
-              <input onClick={this.props.closeApply} type="image" src={Cross} id="cross-img-2"/>
-              <span id="welcome-text3"><br/>Aplikuj na stanowisko!<br/></span>
-              <span id="welcome-text4">Uzupełnij dane i wyślisz formularz. <br/>Odezwiemy się!</span>
-              <div className="d-flex flex-row flex-row2">
-              <form id="form2"> 
-               <span id="email-text-2">Typ zgłoszenia</span>
-                  <select class="form-control2" id="select-menu">
-                   <option>Kurier</option>
-                   <option>Pracownik</option>
-                   <option>Firma Lojalnościowa</option>
-                </select>
-                </form>
-             </div>
-              <div className="d-flex flex-row flex-row2">
-              <form id="form2">    
-               <span id="email-text-2">Imię i nazwisko</span> 
-               <input type="email" className="form-control2" aria-describedby="emailHelp" placeholder="Wprowadz imię i nazwisko"/>
-               </form>
-              </div>
-             <div className="d-flex flex-row flex-row2">
-               <form id="form2">   
-               <span id="email-text-2">Numer telefonu</span> 
-               <input type="email" className="form-control2" aria-describedby="emailHelp" placeholder="Wprowadz numer telefonu"/>
-               </form>
-             </div>
-             <button className="btn btn-primary" id="login-button-2" type="submit">Wyślij</button>
-            </div>
-            </div>,
-            document.getElementById('portal')
-        )
-    }
+  render() {
+    if (this.props.openPopup == false) return null;
+    return ReactDom.createPortal(
+      <div className="gray-cover">
+        <div className="gray-cover__apply-block">
+          <input
+            onClick={this.props.closeApply}
+            type="image"
+            src={Cross}
+            className="gray-cover__apply-block__cross-img"
+          />
+          <span className="gray-cover__apply-block__title">
+            <br />
+            Aplikuj na stanowisko!
+            <br />
+          </span>
+          <span className="gray-cover__apply-block__description">
+            Uzupełnij dane i wyślisz formularz. <br />
+            Odezwiemy się!
+          </span>
+          <div className="d-flex flex-row gray-cover__apply-block__flex-container">
+            <form className="gray-cover__apply-block__flex-container__form">
+              <span className="gray-cover__apply-block__flex-container__form__title">
+                Typ zgłoszenia
+              </span>
+              <select
+                class="form-control2"
+                className="gray-cover__apply-block__flex-container__form__text-input"
+              >
+                <option>Kurier</option>
+                <option>Pracownik</option>
+                <option>Firma Lojalnościowa</option>
+              </select>
+            </form>
+          </div>
+          <div className="d-flex flex-row gray-cover__apply-block__flex-container">
+            <form className="gray-cover__apply-block__flex-container__form">
+              <span className="gray-cover__apply-block__flex-container__form__title">
+                Email
+              </span>
+              <input
+                type="email"
+                className="gray-cover__apply-block__flex-container__form__text-input"
+                aria-describedby="emailHelp"
+                placeholder="Wprowadz email"
+              />
+            </form>
+          </div>
+          <div className="d-flex flex-row gray-cover__apply-block__flex-container">
+            <form className="gray-cover__apply-block__flex-container__form">
+              <span className="gray-cover__apply-block__flex-container__form__title">
+                Imię i nazwisko / Nazwa firmy
+              </span>
+              <input
+                type="email"
+                className="gray-cover__apply-block__flex-container__form__text-input"
+                aria-describedby="emailHelp"
+                placeholder="Wprowadz imię i nazwisko lub nazwę firmy"
+              />
+            </form>
+          </div>
+          <div className="d-flex flex-row gray-cover__apply-block__flex-container">
+            <form className="gray-cover__apply-block__flex-container__form">
+              <span className="gray-cover__apply-block__flex-container__form__title">
+                Numer telefonu
+              </span>
+              <input
+                type="email"
+                className="gray-cover__apply-block__flex-container__form__text-input"
+                aria-describedby="emailHelp"
+                placeholder="Wprowadz numer telefonu"
+              />
+            </form>
+          </div>
+          <button
+            className="btn btn-primary"
+            className="gray-cover__apply-block__send-button"
+            type="submit"
+          >
+            Wyślij
+          </button>
+        </div>
+      </div>,
+      document.getElementById("portal")
+    );
+  }
 }
 export default Apply;
--- react-frontend/delly/src/components/Pop'up/Registration.jsx
+++ react-frontend/delly/src/components/Pop'up/Registration.jsx
@@ -1,50 +1,46 @@
 import React,{Component} from "react";
-import "../../stylesheets/Pop'up/Registration.css"
+import "../../stylesheets/Pop'up/Registration.scss"
 import Cross from "../../images/svg/cross.svg"
 import ReactDom from "react-dom";
 
 class Login extends Component{
-
-    constructor(props) {
-        super(props);
-    }
-    
+  
     render(){
 
         if(this.props.openPopup == false) return null;
         return ReactDom.createPortal(
-            <div id="gray-cover">
-            <div id="login-container"> 
-              <input onClick={this.props.closePopup} type="image" src={Cross} id="cross-img"/>
-              <span id="welcome-text5"><br/>Załóż darmowe konto<br/></span>
-              <div className="d-flex flex-row flex-row3">
-              <form id="form3">    
-               <span id="email-text3">Imie i nazwisko</span> 
-               <input type="email" class="form-control3" aria-describedby="emailHelp" placeholder="Wprowadz imie i nazwisko"/>
+          <div className="gray-cover">
+            <div className="gray-cover__registration-block"> 
+              <input onClick={this.props.closePopup} type="image" src={Cross} className="gray-cover__registration-block__cross-img"/>
+              <span className="gray-cover__registration-block__title"><br/>Załóż darmowe konto<br/></span>
+              <div className="d-flex flex-row gray-cover__registration-block__flex-row">
+              <form className="gray-cover__registration-block__flex-row__form">    
+               <span className="gray-cover__registration-block__flex-row__form__title">Imie i nazwisko</span> 
+               <input type="email" class="gray-cover__registration-block__flex-row__form__input" aria-describedby="emailHelp" placeholder="Wprowadz imie i nazwisko"/>
                </form>
               </div>
-             <div className="d-flex flex-row flex-row3">
-               <form id="form3">   
-               <span id="email-text3">Email</span> 
-               <input type="email" class="form-control3" aria-describedby="emailHelp" placeholder="Wprowadz email"/>
+             <div className="d-flex flex-row gray-cover__registration-block__flex-row">
+               <form className="gray-cover__registration-block__flex-row__form">   
+               <span className="gray-cover__registration-block__flex-row__form__title">Email</span> 
+               <input type="email" class="gray-cover__registration-block__flex-row__form__input" aria-describedby="emailHelp" placeholder="Wprowadz email"/>
                </form>
              </div>
-             <div className="d-flex flex-row flex-row3">
-               <form id="form3">   
-               <span id="email-text3">Hasło</span> 
-               <input type="email" class="form-control3" aria-describedby="emailHelp" placeholder="Wprowadz hasło"/>
+             <div className="d-flex flex-row gray-cover__registration-block__flex-row">
+               <form className="gray-cover__registration-block__flex-row__form">   
+               <span className="gray-cover__registration-block__flex-row__form__title">Hasło</span> 
+               <input type="email" class="gray-cover__registration-block__flex-row__form__input" aria-describedby="emailHelp" placeholder="Wprowadz hasło"/>
                </form>
              </div>
-             <div className="d-flex flex-row flex-row3">
-               <form id="form3">   
-               <span id="email-text3">Powtorz hasło</span> 
-               <input type="email" class="form-control3" aria-describedby="emailHelp" placeholder="powtórz hasło"/>
+             <div className="d-flex flex-row gray-cover__registration-block__flex-row">
+               <form className="gray-cover__registration-block__flex-row__form">   
+               <span className="gray-cover__registration-block__flex-row__form__title">Powtorz hasło</span> 
+               <input type="email" class="gray-cover__registration-block__flex-row__form__input" aria-describedby="emailHelp" placeholder="powtórz hasło"/>
                </form>
              </div>
-             <button class="btn btn-primary" id="login-button3" type="submit">Zarejestruj się</button>
-             <span id="regulations">Poprzez rejestrację zgadzasz się z <span>warunkami<br/> świadczenia usług i polityką prywatności</span></span>
-             <span onClick={this.props.openLogin} id="login-text">Masz już konto? <span>Zaloguj się</span>.</span>
-            </div>
+             <button class="btn btn-primary" className="gray-cover__registration-block__registration-button" type="submit">Zarejestruj się</button>
+             <span className="gray-cover__registration-block__regulations">Poprzez rejestrację zgadzasz się z <span>warunkami<br/> świadczenia usług i polityką prywatności</span></span>
+             <span onClick={this.props.openLogin} className="gray-cover__registration-block__login-text">Masz już konto? <span>Zaloguj się</span>.</span>
+             </div>
             </div>,
             document.getElementById('portal')
         )
