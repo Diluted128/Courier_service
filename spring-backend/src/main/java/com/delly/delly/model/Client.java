54213086c50144efbfb38bd90b5ff02fe882284c
Diluted666 <jacoszekwojciech@gmail.com>
2021-12-17 16:19:23 +0100

    Initial db setup

--- spring-backend/src/main/java/com/delly/delly/model/Client.java
+++ spring-backend/src/main/java/com/delly/delly/model/Client.java
@@ -1,10 +1,81 @@
 package com.delly.delly.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Client {
-    Integer ID;
-    String firstName;
-    String lastName;
-    String phoneNumber;
-    String email;
-    String password;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer ID;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private String email;
+
+    private String password;
+
+    @OneToOne
+    @JoinColumn(name = "credit_card")
+    private CreditCard creditCard;
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public CreditCard getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(CreditCard creditCard) {
+        this.creditCard = creditCard;
+    }
 }
--- spring-backend/src/main/java/com/delly/delly/model/CreditCard.java
+++ spring-backend/src/main/java/com/delly/delly/model/CreditCard.java
@@ -1,10 +1,53 @@
 package com.delly.delly.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.sql.Timestamp;
 
+@Entity
 public class CreditCard {
-    Integer ID;
-    String cardNumber;
-    String CVV;
-    Timestamp expired;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer ID;
+
+    private String cardNumber;
+
+    private String CVV;
+
+    private Timestamp expired;
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCVV() {
+        return CVV;
+    }
+
+    public void setCVV(String CVV) {
+        this.CVV = CVV;
+    }
+
+    public Timestamp getExpired() {
+        return expired;
+    }
+
+    public void setExpired(Timestamp expired) {
+        this.expired = expired;
+    }
 }
